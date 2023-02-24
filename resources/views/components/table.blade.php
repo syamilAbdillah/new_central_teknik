3a2c22652ac75fd2714e302e0088d6c202cdbb02
syamilAbdillah <abdillahsyamil77@gmail.com>
2023-02-25 01:27:04 +0700

    feat: add user list

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,5 @@
+<div class="w-full border rounded-lg overflow-x-auto">
+    <table class="table">
+        {{ $slot }}
+    </table>
+</div>
--- resources/views/dashboard/user/index.blade.php
+++ resources/views/dashboard/user/index.blade.php
@@ -5,7 +5,30 @@
                 <a class="btn btn-success" href="{{ route('users.create') }}" >tambah data pengguna</a>
             </div>
 
-            <p>{{ $users }}</p>
+            <x-table>
+                <thead>
+                    <tr>
+                        <th>no</th>
+                        <th>nama pengguna</th>
+                        <th>email</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($users as $user)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <button class="btn bg-rose-500 text-white hover:bg-rose-400">delete</button>
+                            </td>
+                        </tr>
+                    @empty
+                        
+                    @endforelse
+                </tbody>
+            </x-table>
         </div>
     </div>
 </x-dashboard-layout>
