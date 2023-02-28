93d121d18882f54af956a9d6f0b7a468f825f1ee
syamilAbdillah <abdillahsyamil77@gmail.com>
2023-02-28 18:45:26 +0700

    feat: add about page

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -1,5 +1,16 @@
 <x-catalogue-layout>
     <x-section>
-        <h1 class="text-4xl font-black text-center">tentang kami</h1>
+        <div class="space-y-12">
+            <div class="grid md:grid-cols-3 gap-8">
+                <div class="p-4 flex items-start md:items-center">
+                    <h2 class="text-8xl font-black text-center md:text-left">NCT</h2>
+                </div>
+                <div class="md:col-span-2 p-4 text-xl border border-neutral-700">
+                    <p>New Central Teknik adalah Distributor Penyedia Sparepart Pendingin, dengan berbagai merk Kualitas Terbaik Dengan Harga Bersaing, Serta New Central Teknik menyediakan Jasa Pengadaan,Perbaikan,Serta perawatan Mesin Pendingin dengan Pelayanan yang Memuaskan.</p>
+                    
+                </div>
+            </div>
+        </div>
+
     </x-section>
 </x-catalogue-layout>
